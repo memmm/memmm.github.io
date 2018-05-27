2b2739eddf9fb2dbcceb27d28da11cb83575830d
emesenge <emesenge@gmail.com>
2018-05-27 21:45:03 +0200

    filled portfolio with content

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
 
+
+
   $('.port-item').on('click', function() {
-      $(this).toggleClass('clicked');
+      $('.port-item').removeClass('clicked');
+      $(this).addClass('clicked');
+      $('.extend-port-item').toggle(500);
+
+      var nthChild = $('.port-item').index(this);
+      console.log(nthChild);
+      $('.eitem').css({"display": "none"});
+      switch (nthChild) {
+        case 0:
+          $('.first').css({"display": "flex"});
+          break;
+        case 1:
+          $('.second').css({"display": "flex"});
+          break;
+        case 2:
+          $('.third').css({"display": "flex"});
+          break;
+        case 3:
+          $('.fourth').css({"display": "flex"});
+          break;
+      }
   });
 
   $('.clicked').on('click', function() {
+      //$('.extend-port-item').css({"visibility": "collapse"});
       $(this).removeClass('clicked');
+
   });
+
 
 });
