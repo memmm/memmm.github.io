582707af8b6b9794a97d7d650e8836dcce8cd1df
emesenge <emesenge@gmail.com>
2018-05-28 22:47:26 +0200

    set anchor links to scroll smooth, lniks open in new window

--- script.js
+++ script.js
@@ -39,4 +39,14 @@ $(document).ready(function(){
     $('.links').css({"background": "transparent"});
   });
 
+
+
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
